2d25f69dcc504db746fb6326a76d6b211b4412b3
Frank Tran <franktranvantu@gmail.com>
2021-02-23 23:31:28 +0700

    Update

--- start/script.js
+++ start/script.js
@@ -1,10 +1,13 @@
-// After the object literal, create a new property (using dot OR bracket notation) called characters.
-// This property should be set to the number of characters in the phrase held in the string property.
+// After the object literal, change the value of myString's string property to "I love full stack JavaScript".
+
 const myString = {
-  string: 'Programming with Treehouse is fun!',
-  countWords: function() {
-    const words = this.string.split(' ');
-    return words.length;
+  string: "Programming with Treehouse is fun!",
+  countWords: function(){
+    const wordArray = this.string.split(' ');
+    return wordArray.length;
   }
-};
-const numWords = myString.countWords();
+}
+
+myString.characters = myString.string.length;
+
+var numWords = myString.countWords();
